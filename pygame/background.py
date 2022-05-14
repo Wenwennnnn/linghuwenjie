02e71c16a9bf4d6e0a7a17f1d9cb9ae08705fe01
inghuwenjie <1109946599@qq.com>
2022-05-14 21:36:33 +0800

    初始化游戏

--- pygame/background.py
+++ pygame/background.py
@@ -7,6 +7,12 @@ class player(t.Turtle):
         self.ht()  # 隐藏动画
         self.up()  # 抬起画笔
         self.score = 1  # 设置默认成绩
-# solve a bug
-a=0
-b=0
+        self.color(color) #设置颜色
+        self.speed(0) #设置速度
+        self.goto(goto) #设置默认位置
+        self.shape(shape) #设置形状
+        self.shapesize(5,1) #设置形状比例
+        self.st() #显示动画
+
+def run_game():
+    """初始化游戏，并创建一个屏幕对象"""
